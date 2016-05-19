df2f54f0ecf9d3d4a81f3dc2ade53f41b4f1e90a
Jorge Maroto <patoroco@gmail.com>
2016-05-19 19:20:40 +0200

    2. Move list of cities to an isolated component, and use it in App.js

---
+++ 2TableView/ListOfCities.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+  Image,
+  ListView,
+  Text,
+  TouchableHighlight,
+  View,
+} from 'react-native';
+
+
+const cities = [
+  "Aguilar de Campoó",
+  "Santander",
+  "Valladolid",
+  "Madrid",
+]
+
+
+export default React.createClass({
+  getInitialState: function() {
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
+    return {
+      dataSource: ds.cloneWithRows(cities),
+    };
+  },
+
+  _renderRow(rowData) {
+    return (
+      <TouchableHighlight onPress={() => alert("Touch!")}>
+        {this._rowBody(rowData)}
+      </TouchableHighlight>
+    )
+  },
+
+  _rowBody(rowData) {
+    return (
+      <View
+        style={{ backgroundColor: 'white', height: 50, flexDirection: 'row', alignItems: 'center'}}
+      >
+        <Image
+          source={require('./imgs/picniccode.jpg')}
+          style={{width: 30, height: 30, marginHorizontal: 10}}
+        />
+
+        <Text style={{flex: 1}}>
+          {rowData}
+        </Text>
+      </View>
+    )
+  },
+
+  render() {
+    return (
+      <ListView
+        style={{ flex: 1, backgroundColor: 'gray', paddingTop: 20 }}
+        dataSource={this.state.dataSource}
+        renderRow={this._renderRow}
+      />
+    )
+  },
+})
