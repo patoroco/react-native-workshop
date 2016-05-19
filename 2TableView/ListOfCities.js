68d4f3c853f17aa2f8c2cb5f7822ebdf3900c998
Jorge Maroto <patoroco@gmail.com>
2016-05-19 21:03:59 +0200

    2. Extract cities array to an external file

--- 2TableView/ListOfCities.js
+++ 2TableView/ListOfCities.js
@@ -8,12 +8,7 @@ import {
 } from 'react-native';
 
 
-const cities = [
-  {cityName: "Aguilar de Campoó", wikiURL: "https://es.wikipedia.org/wiki/Aguilar_de_Campoo"},
-  {cityName: "Santander", wikiURL: "https://es.wikipedia.org/wiki/Santander_(Espa%C3%B1a)"},
-  {cityName: "Valladolid", wikiURL: "https://es.wikipedia.org/wiki/Valladolid"},
-  {cityName: "Madrid", wikiURL: "https://es.wikipedia.org/wiki/Madrid"},
-]
+import cities from './cities'
 
 
 export default React.createClass({
---
+++ 2TableView/cities.js
@@ -0,0 +1,6 @@
+export default [
+  {cityName: "Aguilar de Campoó", wikiURL: "https://es.wikipedia.org/wiki/Aguilar_de_Campoo"},
+  {cityName: "Santander", wikiURL: "https://es.wikipedia.org/wiki/Santander_(Espa%C3%B1a)"},
+  {cityName: "Valladolid", wikiURL: "https://es.wikipedia.org/wiki/Valladolid"},
+  {cityName: "Madrid", wikiURL: "https://es.wikipedia.org/wiki/Madrid"},
+]
