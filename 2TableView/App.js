2bd57689fb9e382d36f862ee1e985aa0ece977d6
Jorge Maroto <patoroco@gmail.com>
2016-05-19 19:25:48 +0200

    2. Add navigator and, include cities list as first scene

--- 2TableView/App.js
+++ 2TableView/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  View,
+  Navigator,
 } from 'react-native';
 
 import ListOfCities from './ListOfCities'
@@ -8,7 +8,20 @@ import ListOfCities from './ListOfCities'
 export default React.createClass({
   render() {
     return (
-      <ListOfCities />
+      <Navigator
+        style={{flex: 1}}
+        initialRoute={{name: 'cities'}}
+        configureScene={(route) => Navigator.SceneConfigs.FloatFromRight}
+        renderScene={this._routeMapper}
+      />
     )
   },
+
+  _routeMapper(route, navigator) {
+    if (route.name === 'cities') {
+      return <ListOfCities />
+    }
+
+    return null
+  }
 })
