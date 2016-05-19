f84d9e1910defd26775c75c17cb0d0df0f84f567
Jorge Maroto <patoroco@gmail.com>
2016-05-19 19:28:23 +0200

    2. Create a new component to show city detail

---
+++ 2TableView/CityDetail.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {
+  Image,
+  Text,
+  View,
+} from 'react-native'
+
+
+export default React.createClass({
+  render() {
+    return <View style={{ flex: 1, backgroundColor: 'green'}} />
+  }
+})
