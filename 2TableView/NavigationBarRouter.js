a10161176a614435ed1c547ea545c50116df27e2
Jorge Maroto <patoroco@gmail.com>
2016-05-19 20:10:08 +0200

    2. Back button now pop

--- 2TableView/NavigationBarRouter.js
+++ 2TableView/NavigationBarRouter.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import {
   Text,
+  TouchableHighlight,
+  View,
 } from 'react-native'
 
 export default {
   LeftButton: function(route, navigator, index, navState) {
     if (route.name === 'detail') {
       return (
-        <Text>
-          Back
-        </Text>
+        <TouchableHighlight onPress={() => navigator.pop()}>
+          <Text>Back</Text>
+        </TouchableHighlight>
       )
     }
 
