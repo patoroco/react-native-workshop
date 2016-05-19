40a6dd732482b49cf288b973b074d994e60395a6
Jorge Maroto <patoroco@gmail.com>
2016-05-19 18:46:47 +0200

    2. Refactor cities array and _renderRow method

--- 2TableView/App.js
+++ 2TableView/App.js
@@ -5,19 +5,35 @@ import {
 } from 'react-native';
 
 
+const cities = [
+  "Aguilar de Campoó",
+  "Santander",
+  "Valladolid",
+  "Madrid",
+]
+
+
 export default React.createClass({
   getInitialState: function() {
     var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
     return {
-      dataSource: ds.cloneWithRows(["Aguilar de Campoó", "Santander", "Valladolid", "Madrid"]),
+      dataSource: ds.cloneWithRows(cities),
     };
   },
 
-  render: function() {
+  _renderRow(rowData) {
+    return (
+      <Text>
+        {rowData}
+      </Text>
+    )
+  },
+
+  render() {
     return (
       <ListView
         dataSource={this.state.dataSource}
-        renderRow={(rowData) => <Text>{rowData}</Text>}
+        renderRow={this._renderRow}
       />
     )
   },
