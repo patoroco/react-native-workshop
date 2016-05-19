632ad57846f312675bb0f605f3d7d94a1ae488f3
Jorge Maroto <patoroco@gmail.com>
2016-05-19 19:42:40 +0200

    2. Add default props to cityName in detail scene

--- 2TableView/CityDetail.js
+++ 2TableView/CityDetail.js
@@ -7,6 +7,12 @@ import {
 
 
 export default React.createClass({
+  getDefaultProps() {
+    return {
+      cityName: 'placeholder',
+    }
+  },
+
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'green'}}>
