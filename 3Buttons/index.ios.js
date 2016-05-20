219703bf22f8b6a74bb26a189e79a4c4149164e3
Jorge Maroto <patoroco@gmail.com>
2016-05-20 08:19:48 +0200

    3. Modify state on response with IP

--- 3Buttons/index.ios.js
+++ 3Buttons/index.ios.js
@@ -16,10 +16,15 @@ import APIClient from './APIClient'
 
 
 class Buttons extends Component {
+  constructor(props) {
+     super(props);
+     this.state = { ip: 'undefined' };
+   }
+
   componentDidMount() {
     let client = new APIClient('https://httpbin.org')
     client.get('ip')
-      .then(json => alert(json.origin))
+      .then(json => this.setState({ ip: json.origin }))
       .catch((error) => console.error(error))
   }
 
@@ -27,14 +32,7 @@ class Buttons extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          {this.state.ip}
         </Text>
       </View>
     );
